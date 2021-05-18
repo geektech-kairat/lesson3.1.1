c170b5ffef98866f4ec4ef5f5925a87d79af5e8f
kairat <goold1999@mail,,ru>
2021-05-18 21:15:25 +0600

    last

--- app/src/main/java/com/example/lesson31/domain/Game.java
+++ app/src/main/java/com/example/lesson31/domain/Game.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static android.content.ContentValues.TAG;
 
@@ -16,7 +20,9 @@ public class Game<C> {
         for (int i = 0; i < cardContent.size(); i++) {
             cards.add(new Card<>(i + 1, false, false, cardContent.get(i)));
             cards.add(new Card<>((i + 1) * 2, false, false, cardContent.get(i)));
-
+            cards.add(new Card<>((i + 1) * 4, false, false, cardContent.get(i)));
+            cards.add(new Card<>((i + 1) * 3, false, false, cardContent.get(i)));
+            Collections.shuffle(cards);
         }
 
     }
@@ -27,21 +33,27 @@ public class Game<C> {
     }
 
     public void CardClick(Card<C> card, Context context) {
+
         card.setFaceAUp(!card.isFaceAUp());
-        Log.e(TAG, "CardClick: ");
         for (Card<C> currentCard : cards) {
-            if (currentCard.getId() != card.getId() && currentCard.isFaceAUp() && currentCard.getCardContent().equals(card.getCardContent())) {
-                Toast.makeText(context, "Совпадения карт)\n карты исчезает", Toast.LENGTH_SHORT).show();
-                currentCard.setMatched(true);
-                card.setMatched(true);
+            if (currentCard.isFaceAUp() && currentCard.getId() != card.getId()) {
+                if (currentCard.getCardContent().equals(card.getCardContent())) {
+                    Toast.makeText(context, "Совпадения карт)\n карты исчезает", Toast.LENGTH_SHORT).show();
+
+                    currentCard.setMatched(true);
+                    card.setMatched(true);
+                } else {
+                    currentCard.setFaceAUp(!currentCard.isFaceAUp());
+                    card.setFaceAUp(!card.isFaceAUp());
+                }
             }
-//            else if (currentCard.getCardContent().equals(card.getCardContent())){
-//                Toast.makeText(context, "Совпадении карт нет)\n ", Toast.LENGTH_SHORT).show();
-//
-//            }
+
         }
         remove();
+
+
     }
+
     private void remove() {
         List<Card<C>> resultCards = new ArrayList<>(cards);
         for (Card<C> c : cards) {
@@ -54,7 +66,9 @@ public class Game<C> {
 
     }
 
-
 }
+
+
+
 
 
--- app/src/main/java/com/example/lesson31/presentation/EmojiGame.java
+++ app/src/main/java/com/example/lesson31/presentation/EmojiGame.java
@@ -7,21 +7,23 @@ import com.example.lesson31.domain.Card;
 import com.example.lesson31.domain.Game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class EmojiGame {
-    private final Game<String> game;
+    private  Game<String> game;
     public EmojiGame (){
         List<String> cardContent = new ArrayList<>();
         cardContent.add("😎");
         cardContent.add("😂");
-        cardContent.add("😍");
-        cardContent.add("🙈");
+        cardContent.add("😊");
+        cardContent.add(" \uD83D\uDC80");
         game = new Game<>(cardContent);
     }
     public void cardClick(Card<String> card, Context context){
         game.CardClick(card, context);
     }
+
     public List<Card<String>> getCards(){
         return  game.getCards();
     }
